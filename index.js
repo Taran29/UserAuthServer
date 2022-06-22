961e2ba44758b7b437b1f2a7d6f165d6de3fa1ca
Taranpreet <taranpreetsingh29@gmail.com>
2022-06-22 17:12:44 +0530

    Added auth token to set of accepted headers

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
   next();
 });
 app.use('/api/register', register)
